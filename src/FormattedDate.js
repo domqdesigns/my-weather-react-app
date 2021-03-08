103a9fab7ff619932632aada1e236b930e589226
domqdesigns <domqdesigns@gmail.com>
2021-03-08 11:38:17 -0500

    Update FormattedDate.js

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -5,11 +5,11 @@ export default function FormattedDate(props){
     let day = days[props.date.getDay()];
     let hours = props.hours.getHours();
     if (hours < 10){
-        hours = '0${hours}';
+        hours = "0"+ hours;
     }
     let minutes = props.minutes.getMinutes();
     if (minutes < 10){
-        minutes = '0${minutes}';
+        minutes = "0"+ minutes;
     }
  
     return <div>
